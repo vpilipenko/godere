0a7bd01ecd4984c3a346e4be4afda6b7a229a987
V.Pilipenko <v.pilipenko@avtopass.net>
2020-04-03 22:28:16 +0300

    config

---
+++ dist/assets/js/main-min.js
@@ -0,0 +1,2 @@
+"use strict";var $header=document.getElementsByTagName("header")[0];$header.addEventListener("click",(function(){console.log("click on header")}));
+//# sourceMappingURL=main-min.js.map
---
+++ dist/components/header/header-min.js
@@ -0,0 +1,2 @@
+"use strict";var $header=document.getElementsByTagName("header");$header.addEventListener("click",(function(e){console.log("click on header")}));
+//# sourceMappingURL=header-min.js.map
