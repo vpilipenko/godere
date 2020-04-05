c7c3b11fc1ba4779959041d74a3640c169dfff94
Александр Агафонов <a.agafonov@avto-pass.net>
2020-04-05 14:06:28 +0300

    add footer

--- src/components/footer/footer.php
+++ src/components/footer/footer.php
@@ -1,3 +1,34 @@
 <footer class='godere-footer'>
-  Footer
+  <div class='godere-footer__col'>
+    <? include 'components/logo/logo.php' ?>
+    <div class='godere-footer__copy'>2020 ООО «Годере»</div>
+    <a class='godere-footer__user-agreement' href='#'>
+      Пользовательское соглашение
+    </a>
+  </div>
+  <div class='godere-footer__col'>
+    <div class="godere-footer__title">Клиентам</div>
+    <a href="#" class="godere-footer__link">Обратная связь</a>
+    <a href="#" class="godere-footer__link">Заказы</a>
+    <a href="#" class="godere-footer__link">О нас</a>
+  </div>
+  <div class='godere-footer__col'>
+    <div class="godere-footer__title">Как купить</div>
+    <a href="#" class="godere-footer__link">Оплата и доставка</a>
+    <a href="#" class="godere-footer__link">Возврат</a>
+  </div>
+  <div class='godere-footer__col'>
+    <div class="godere-footer__title">Партнерам</div>
+    <a href="#" class="godere-footer__link">Начать сотрудничество</a>
+    <a href="#" class="godere-footer__link">Партрнерская программа</a>
+  </div>
+  <div class='godere-footer__col'>
+    <div class="godere-footer__title">Мы в соцсетях</div>
+    <a href="#" class="godere-footer__social-link">
+      <img src="/src/assets/img/vk.png" alt="" class="godere-footer__social-img">
+    </a>
+    <a href="#" class="godere-footer__social-link">
+      <img src="/src/assets/img/fb.png" alt="" class="godere-footer__social-img">
+    </a>
+  </div>
 </footer>
