ff813ef42ce9e4d35d5e3cd7501652113f710b65
Александр Агафонов <a.agafonov@avto-pass.net>
2020-04-06 14:20:07 +0300

    Add success & checkout

--- dist/checkout.php
+++ dist/checkout.php
@@ -15,15 +15,50 @@
 	<link rel="stylesheet" href="/assets/styles/main-min.css">
 </head>
 <body>
+<div class="container godere-container">
+	<div class="godere">
+		<!-- Header -->
+		<? include 'components/header/header.php' ?>
+		<!-- Header end -->
 
-<!-- Header -->
-<? include 'components/header/header.php' ?>
-<!-- Header end -->
+		<!-- Breadcrumbs -->
+		<? include 'components/breadcrumbs/breadcrumbs.php' ?>
+		<!-- Breadcrumbs end -->
 
+		<h2>Корзина</h2>
+		<!-- Checkout -->
+		<div class="godere-checkout">
+			
+			<div class="godere-checkout__content">
+				<div class="godere-checkout__item">
+					<div class="godere-checkout__title">Контактная информация</div>
+					<input type="text"><input type="text">
+				</div>
+				<div class="godere-checkout__item">
+					<div class="godere-checkout__title">Время доставки</div>
+					<select name="" id=""></select>
+				</div>
+				<div class="godere-checkout__item">
+					<div class="godere-checkout__title">Адрес</div>
+					<input type="text"><br>
+					<input type="text"><input type="text"><br>
+					<input type="text"><input type="text"><br>
+					<textarea name="" id="" cols="30" rows="10"></textarea>
+				</div>
+				<div class="godere-checkout__item">
+					<div class="godere-checkout__title">Способ оплаты</div>
+					<input type="radio">Наличными курьеру
+					<input type="radio">Картой
+				</div>
+			</div>
+			<? include 'components/form/form.php' ?>
+		</div>
+		<!-- Checkout end -->
+	<!-- Footer -->
+	<? include 'components/footer/footer.php' ?>
+	<!-- Footer end -->
+</div>
 
-<!-- Footer -->
-<? include 'components/footer/footer.php' ?>
-<!-- Footer end -->
 
 <!-- JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- src/success.php
+++ src/success.php
@@ -15,15 +15,99 @@
 	<link rel="stylesheet" href="/assets/styles/main-min.css">
 </head>
 <body>
+<div class="container godere-container">
+	<div class="godere">
+		<!-- Header -->
+		<? include 'components/header/header.php' ?>
+		<!-- Header end -->
 
-<!-- Header -->
-<? include 'components/header/header.php' ?>
-<!-- Header end -->
+		<!-- Breadcrumbs -->
+		<? include 'components/breadcrumbs/breadcrumbs.php' ?>
+		<!-- Breadcrumbs end -->
 
+		<!-- Success -->
+		<div class="godere-success">
+			<div class="godere-success__content">
+				<span class="godere-success__number">Заказ 935 578</span>
+				<span class="godere-success__date">от 3.03.2020</span>
+				<div class="godere-success__pay">Оплата курьеру наличными</div>
 
-<!-- Footer -->
-<? include 'components/footer/footer.php' ?>
-<!-- Footer end -->
+				<div class="godere-success__item">
+					<div class="godere-success__title">Адрес:</div>
+					<div class="godere-success__value">Москва, ул. Островитянова,  д 20, кв. 35</div>
+				</div>
+				<div class="godere-success__item">
+					<div class="godere-success__title">Получение:</div>
+					<div class="godere-success__value">Доставка курьером, 04.03.2020 (завтра), с 10:00 до 18:00</div>
+				</div>
+				<div class="godere-success__item">
+					<div class="godere-success__title">Получатель:</div>
+					<div class="godere-success__value">Сергей Смирнов +7 925 123-45-56</div>
+				</div>
+
+				<div class="godere-success__progress">Прогресс доставки</div>
+
+				<div class="godere-cart">
+			<ul class="godere-cart__list">
+				<li class="godere-cart__item">
+				<img class="godere-cart__img" src="/src/assets/img/cart.png" alt="cart">
+				<div class="godere-cart__txt">
+					<div class="godere-cart__title">Вибратор ABBY G фиолетовый</div>
+					<div class="godere-cart__description">Вибратор Abby G – один из 
+					представителей линейки Battery +.
+					</div>
+				</div>
+				<div class="godere-cart__quantity">
+					<div class="godere-cart__minus">-</div>
+					<div class="godere-cart__amount">3</div>
+					<div class="godere-cart__plus">+</div>
+				</div>
+				<div class="godere-cart__price">3 240 ₽</div>
+				<div class="godere-cart__close">×</div>
+				</li>
+				<li class="godere-cart__item">
+				<img class="godere-cart__img" src="/src/assets/img/cart.png" alt="cart">
+				<div class="godere-cart__txt">
+					<div class="godere-cart__title">Вибратор ABBY G фиолетовый</div>
+					<div class="godere-cart__description">Вибратор Abby G – один из 
+					представителей линейки Battery +.
+					</div>
+				</div>
+				<div class="godere-cart__quantity">
+					<div class="godere-cart__minus">-</div>
+					<div class="godere-cart__amount">3</div>
+					<div class="godere-cart__plus">+</div>
+				</div>
+				<div class="godere-cart__price">3 240 ₽</div>
+				<div class="godere-cart__close">×</div>
+				</li>
+				<li class="godere-cart__item">
+				<img class="godere-cart__img" src="/src/assets/img/cart.png" alt="cart">
+				<div class="godere-cart__txt">
+					<div class="godere-cart__title">Вибратор ABBY G фиолетовый</div>
+					<div class="godere-cart__description">Вибратор Abby G – один из 
+					представителей линейки Battery +.
+					</div>
+				</div>
+				<div class="godere-cart__quantity">
+					<div class="godere-cart__minus">-</div>
+					<div class="godere-cart__amount">3</div>
+					<div class="godere-cart__plus">+</div>
+				</div>
+				<div class="godere-cart__price">3 240 ₽</div>
+				<div class="godere-cart__close">×</div>
+				</li>
+			</ul>
+			</div>
+		</div>
+			<? include 'components/form/form.php' ?>
+		</div>
+		<!-- Success end -->
+	<!-- Footer -->
+	<? include 'components/footer/footer.php' ?>
+	<!-- Footer end -->
+</div>
+
 
 <!-- JavaScript -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
